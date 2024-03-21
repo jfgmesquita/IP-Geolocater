8ac58bb84d7d3796c2afb1aed9bd95a0dd821a8f
João Mesquita <151409184+bacon720kg@users.noreply.github.com>
2024-03-21 00:40:54 +0000

    Add files via upload

---
+++ Geolocalização de IP's.py
@@ -0,0 +1,32 @@
+import requests, streamlit, pandas, ipaddress
+
+streamlit.set_page_config(page_title = "Geolocalização de IP's")
+streamlit.write("# Geolocalização de IP's")
+
+with streamlit.form(key = "Qualquer coisa"):
+    user_input = streamlit.text_input("Introduza o IP a localizar:")
+    submit_button = streamlit.form_submit_button(label = "Submeter")
+    if submit_button == True:
+        try:
+            ipaddress.ip_address(user_input)
+            output = "O IP introduzido foi " + user_input
+            streamlit.write(output)
+
+            info_ip = requests.get(f"https://ipinfo.io/{user_input}/json")
+
+            latitude = info_ip.json()["loc"].split(",")[0]
+            longitude = info_ip.json()["loc"].split(",")[1]
+
+            info_meteo = requests.get(f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m,wind_speed_10m")
+
+            ip = info_ip.json()["ip"]
+
+            streamlit.write("## Veja abaixo a localização do IP introduzido.")
+            data = pandas.DataFrame ({"latitude": [float(latitude)], "longitude": [float(longitude)]})
+
+            streamlit.map(data,use_container_width = True)
+        except:
+            streamlit.write("O formato de IP introduzido é inválido.")
+
+# Depois de correr o ficheiro, escrever separadamente no terminal: pip install requests, pip install streamlit, pip install ipadrass
+# streamlit run "c:/Users/jolit/OneDrive/Ambiente de Trabalho/Geolocalização de IP's.py" - As aspas são necessárias porque o ficheiro está no OneDrive
